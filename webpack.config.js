8f43a8ea6f21b784eaee37353a13976c75df1068
Daniel Cavalcante <danielcavalcante@MacBook-Pro-de-Daniel.local>
2018-09-25 00:41:33 -0300

    Add libs and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,47 @@
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: './ex/index.jsx',
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx']
-  },
+  entry: './src/index.jsx',
   output: {
     path: __dirname + '/public',
     publicPath: '/',
     filename: 'bundle.js'
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
   devServer: {
     contentBase: './public',
-    hot: true
-  }
+    hot: true,
+    port: 8080
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+    alias: {
+      modules: __dirname + '/node_modules'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+        query: {
+          presets: ['es2015', 'react'],
+          plugins: ['transform-object-rest-spread']
+        }
+      }, 
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract('style-loader', 'css-loader')
+      },
+      {
+        test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
+        use: 'file'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new ExtractTextPlugin('app.css')
+  ]
 };
