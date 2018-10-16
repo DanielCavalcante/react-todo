e69bdae1ffa6a1f8e716a574390225f6a63f3765
Daniel Cavalcante <c0500272@vale.com>
2018-10-16 13:43:56 -0300

    Add evento for input and button

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -7,19 +7,29 @@ import TodoList from './todoList';
 export default class Todo extends Component {
   constructor(props) {
     super(props);
+    this.state = { description: '', list: []};
+    this.handleChange = this.handleChange.bind(this);
     this.handleAdd = this.handleAdd.bind(this);
   };
 
-  handleAdd() {
-    console.log(this);
+  handleChange(e) {
+    this.setState({...this.state, description: e.target.value})
   };
+
+  handleAdd() {
+    console.log(this.state.description);
+  };
+
   render() {
     return (
       <div>
         <PageHeader name='Tasks' small='Create'></PageHeader>
-        <TodoForm handleAdd={this.handleAdd} />
+        <TodoForm 
+          description={this.state.description} 
+          handleAdd={this.handleAdd}
+          handleChange={this.handleChange} />
         <TodoList />
       </div>
     );
-  }
+  };
 };
