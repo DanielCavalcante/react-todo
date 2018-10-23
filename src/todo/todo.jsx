d5969815d6f887b3e9f4ab578283d011273f7977
Daniel Cavalcante <c0500272@vale.com>
2018-10-23 13:38:15 -0300

    Add update todo and custom css

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -14,6 +14,8 @@ export default class Todo extends Component {
     this.handleChange = this.handleChange.bind(this);
     this.handleAdd = this.handleAdd.bind(this);
     this.handleRemove = this.handleRemove.bind(this);
+    this.handleMarkAsDone = this.handleMarkAsDone.bind(this);
+    this.handleMarkAsPending = this.handleMarkAsPending.bind(this);
 
     this.refresh();
   };
@@ -41,6 +43,18 @@ export default class Todo extends Component {
     });
   };
 
+  handleMarkAsDone(todo) {
+    axios.put(`${API}/${todo._id}`, {...todo, done: true}).then(res => {
+      this.refresh();
+    });
+  };
+
+  handleMarkAsPending(todo) {
+    axios.put(`${API}/${todo._id}`, {...todo, done: false}).then(res => {
+      this.refresh();
+    });
+  };
+
   render() {
     return (
       <div>
@@ -49,7 +63,11 @@ export default class Todo extends Component {
           description={this.state.description} 
           handleAdd={this.handleAdd}
           handleChange={this.handleChange} />
-        <TodoList list={this.state.list} handleRemove={this.handleRemove} />
+        <TodoList list={this.state.list}
+          handleRemove={this.handleRemove}
+          handleMarkAsDone={this.handleMarkAsDone}
+          handleMarkAsPending={this.handleMarkAsPending}>
+        </TodoList>
       </div>
     );
   };
